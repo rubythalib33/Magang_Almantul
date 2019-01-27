bdccb4db9af6aedad3d8f2817416943979fe6dba
TaufiqOdhi <taufiq_0dhi_dwi_putra@student.uns.ac.id>
2019-01-27 17:54:37 +0700

    mengimport database ke project

--- PosBiptek/app/src/main/java/com/biptek/posbiptek/MainActivity.java
+++ PosBiptek/app/src/main/java/com/biptek/posbiptek/MainActivity.java
@@ -1,13 +1,40 @@
 package com.biptek.posbiptek;
 
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDBHelper = new DatabaseHelper(this);
+
+        //Hanya untuk mengetahui direktori database disimpan
+        TextView textView = findViewById(R.id.textview1);
+        textView.append(getApplicationContext().getApplicationInfo().dataDir);
+
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
+
+
     }
 }
