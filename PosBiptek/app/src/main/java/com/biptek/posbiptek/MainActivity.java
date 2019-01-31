c91bd8b0a82bdab0c2dad8258b5344cf6367160c
TaufiqOdhi <taufiq_0dhi_dwi_putra@student.uns.ac.id>
2019-01-31 15:17:04 +0700

    layout rev

--- PosBiptek/app/src/main/java/com/biptek/posbiptek/MainActivity.java
+++ PosBiptek/app/src/main/java/com/biptek/posbiptek/MainActivity.java
@@ -17,40 +17,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_admin);
 
-        mDBHelper = new DatabaseHelper(this);
-        TextView textView = findViewById(R.id.textView);
-        textView.append(this.getApplicationInfo().dataDir+"\n");
-
-        try {
-            mDBHelper.updateDataBase();
-        } catch (IOException mIOException) {
-            throw new Error("UnableToUpdateDatabase");
-        }
-
-        CRUD crud = new CRUD(this);
-        crud.open();
-        crud.addProduk(new Produk("2",
-                "jasa",
-                "komputer",
-                "Install Linux OS",
-                "Ubuntu 18.10",
-                50000,
-                0,
-                "unit",
-                "-",
-                30,
-                0,
-                "baru"));
-        User user = crud.getUser("odhi");
-        List<ListProdukTerjual> produkTerjuals = new ArrayList<>();
-        TransaksiPenjualan transaksiPenjualan = new TransaksiPenjualan(2, user.getUsername(), "putra", "hari ini");
-        produkTerjuals.add(new ListProdukTerjual(transaksiPenjualan.getKode_penjualan(), "1", 11));
-        produkTerjuals.add(new ListProdukTerjual(transaksiPenjualan.getKode_penjualan(), "2", 2));
-        crud.deleteTransaksiPenjualan(transaksiPenjualan);
-        textView.append(crud.getAllTransaksiPenjualan().toString());
-        //textView.append(crud.getListProdukTerjual(transaksiPenjualan.getKode_penjualan()).toString());
-        crud.close();
+//        mDBHelper = new DatabaseHelper(this);
+//        TextView textView = findViewById(R.id.textView);
+//        textView.append(this.getApplicationInfo().dataDir+"\n");
+//
+//        try {
+//            mDBHelper.updateDataBase();
+//        } catch (IOException mIOException) {
+//            throw new Error("UnableToUpdateDatabase");
+//        }
+//
+//        CRUD crud = new CRUD(this);
+//        crud.open();
+//        crud.addProduk(new Produk("2",
+//                "jasa",
+//                "komputer",
+//                "Install Linux OS",
+//                "Ubuntu 18.10",
+//                50000,
+//                0,
+//                "unit",
+//                "-",
+//                30,
+//                0,
+//                "baru"));
+//        User user = crud.getUser("odhi");
+//        List<ListProdukTerjual> produkTerjuals = new ArrayList<>();
+//        TransaksiPenjualan transaksiPenjualan = new TransaksiPenjualan(2, user.getUsername(), "putra", "hari ini");
+//        produkTerjuals.add(new ListProdukTerjual(transaksiPenjualan.getKode_penjualan(), "1", 11));
+//        produkTerjuals.add(new ListProdukTerjual(transaksiPenjualan.getKode_penjualan(), "2", 2));
+//        crud.deleteTransaksiPenjualan(transaksiPenjualan);
+//        textView.append(crud.getAllTransaksiPenjualan().toString());
+//        //textView.append(crud.getListProdukTerjual(transaksiPenjualan.getKode_penjualan()).toString());
+//        crud.close();
     }
 }
