18afa3261aa845b17c0df9865c47cdcfe3cdf98a
Unknown <rubyabdullah@student.uns.ac.id>
2019-02-19 08:13:03 +0700

    mantul

---
+++ Prototype/src/prototype/Prototype.java
@@ -0,0 +1,29 @@
+package prototype;
+
+class Data{
+	
+}
+public class Prototype {
+	
+	void  input() {
+		
+	}
+	void dist(Data a, Data b) {
+		
+	}
+	void generate_centroid() {
+		
+	}
+	void kmeans() {
+		
+	}
+	void cluster() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
