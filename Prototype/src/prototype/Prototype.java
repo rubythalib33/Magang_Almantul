f1f107d0e98b2353864040a5ffc4d0e706b96729
rubythalib33 <abdullah3333abdul@gmail.com>
2019-02-26 09:47:56 +0700

    Update prototype lagi

--- Prototype/src/prototype/Prototype.java
+++ Prototype/src/prototype/Prototype.java
@@ -1,5 +1,6 @@
 package prototype;
 import java.util.Random;
+import java.util.ArrayList;
 
 public class Prototype {
 	Random rand = new Random();
@@ -17,7 +18,7 @@ public class Prototype {
 			 "Madu Al-Kautsar","Madu Batuk Al-Wadey","Madu for Mama Al Mabruroh",
 			 "Madu Hitam Pahit 460 gram","Madu Hutan Liar Al Wadey 500 Gram",
 			 "Madu Hutan Sialang 1 Kg"};
-	double[][] c = {{109.,41.,54.16666667},
+	Double[][] c = {{109.,41.,54.16666667},
 			 { 99.,16.,55.        },
 			 { 85.,16.,50.        },
 			 { 81.,18.,50.        },
@@ -40,14 +41,15 @@ public class Prototype {
 			 { 92.,42.,52.94117647},
 			 {112.,49.,47.91666667},
 			 {117.,18.,52.        }};
+	int[] clusters=new int[c.length];
 	
 	void  input() {
 		
 	}
-	double[] dist(int[] a, int[][] b) {
-		double[] temp = new double[k];
+	Double[] dist(Double[] a, Double[][] b) {
+		Double[] temp = new Double[k];
 		for(int i=0;i<temp.length;i++) {
-			double temp2 =0;
+			Double temp2 =0.0;
 			for(int j=0;j<3;j++) {
 				temp2+=Math.pow(a[j]-b[i][j],2);
 			}
@@ -55,8 +57,8 @@ public class Prototype {
 		}
 		return null;
 	}
-	double[][] generate_centroid() {
-		double[][] centroid= new double[k][3];
+	Double[][] generate_centroid() {
+		Double[][] centroid= new Double[k][3];
 		for (int i=0;i < k; i++) {
 			for(int j=0; j<3; j++) {
 				centroid[i][j]=rand.nextInt(75)+rand.nextDouble();
@@ -65,7 +67,34 @@ public class Prototype {
 		return centroid;
 	}
 	void kmeans() {
+		Double[][] centroid = generate_centroid();
+		Double[][] centroid_old = new Double[4][3];
+		int count =0;
+		while(count<100) {
+			for(int i=0 ; i<c.length;i++) {
+				Double[] distance=dist(c[i],centroid);
+				clusters[i]=argmin(distance);
+			}
+			centroid_old=centroid;
+			for(int i=0; i<k;i++) {
+				ArrayList<Double[]> points = new ArrayList<Double[]>();
+				for(int j=0;j<c.length;j++) {
+					if (clusters[j]==i) {
+						points.add(c[j]);
+					}
+				}
+			}
+		}
 		
+	}
+	int argmin(Double[] input) {
+		int temp=0;
+		for(int i=0;i<input.length;i++) {
+			if(input[i]<input[temp]) {
+				temp=i;
+			}
+		}
+		return temp;
 	}
 	void cluster() {
 		
