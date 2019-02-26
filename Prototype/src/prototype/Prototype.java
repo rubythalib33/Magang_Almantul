250d67d2bff66df8e469f8a3af364a1dc3bfdb5b
rubythalib33 <abdullah3333abdul@gmail.com>
2019-02-26 09:13:58 +0700

    Update prototype

--- Prototype/src/prototype/Prototype.java
+++ Prototype/src/prototype/Prototype.java
@@ -1,9 +1,22 @@
 package prototype;
-
+import java.util.Random;
 
 public class Prototype {
+	Random rand = new Random();
+	
+	int k = 4;
 	float[][] a = new float[23][5];
-	String[] b = new String[23];
+	String[] b = {"Habbatussauda Extra Propolis Trigon","Habbatussauda Oil 200 Kapsul",
+			 "Habasyi Oil 100 Kps","Habasyi Oil 210 Kps","Kapsul Habbatus Sauda",
+			 "Habat Oil 369","Habbatussauda Al-Afiat 100 Kps",
+			 "Habbatussauda Al-Afiat 200 Kps","Habbatussauda Oil Al-Afiat 100 Kps",
+			 "Habbatussauda Oil Al-Afiat 200 Kps","Habbatussauda Oil Al-Afiat 60 Kps",
+			 "Propolis Brazalian / Botol","Propolis Diamond Lite 20 / Botol",
+			 "Propolis Diamond Regular / Botol","Propolis Melia Nature / Botol",
+			 "Propolis Melia Sehat / Botol","Propolis Prosmart/ Botol",
+			 "Madu Al-Kautsar","Madu Batuk Al-Wadey","Madu for Mama Al Mabruroh",
+			 "Madu Hitam Pahit 460 gram","Madu Hutan Liar Al Wadey 500 Gram",
+			 "Madu Hutan Sialang 1 Kg"};
 	double[][] c = {{109.,41.,54.16666667},
 			 { 99.,16.,55.        },
 			 { 85.,16.,50.        },
@@ -27,17 +40,29 @@ public class Prototype {
 			 { 92.,42.,52.94117647},
 			 {112.,49.,47.91666667},
 			 {117.,18.,52.        }};
-	float[][] centroid = new float[4][3];
-	
 	
 	void  input() {
 		
 	}
-	void dist(int[][] a, int[][] b) {
-		
+	double[] dist(int[] a, int[][] b) {
+		double[] temp = new double[k];
+		for(int i=0;i<temp.length;i++) {
+			double temp2 =0;
+			for(int j=0;j<3;j++) {
+				temp2+=Math.pow(a[j]-b[i][j],2);
+			}
+			temp[i]=Math.sqrt(temp2); 
+		}
+		return null;
 	}
-	void generate_centroid() {
-		
+	double[][] generate_centroid() {
+		double[][] centroid= new double[k][3];
+		for (int i=0;i < k; i++) {
+			for(int j=0; j<3; j++) {
+				centroid[i][j]=rand.nextInt(75)+rand.nextDouble();
+			}
+		}
+		return centroid;
 	}
 	void kmeans() {
 		
@@ -47,7 +72,7 @@ public class Prototype {
 	}
 
 	public static void main(String[] args) {
-		
+		System.out.println();
 
 	}
 
