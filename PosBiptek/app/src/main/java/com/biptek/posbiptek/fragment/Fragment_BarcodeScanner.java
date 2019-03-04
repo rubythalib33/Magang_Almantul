fb64e7f60aef98539e1c60695242c3e19209bd65
TaufiqOdhi <taufiq_0dhi_dwi_putra@student.uns.ac.id>
2019-03-04 23:19:58 +0700

    penyesuaian database baru

--- PosBiptek/app/src/main/java/com/biptek/posbiptek/fragment/Fragment_BarcodeScanner.java
+++ PosBiptek/app/src/main/java/com/biptek/posbiptek/fragment/Fragment_BarcodeScanner.java
@@ -1,95 +1,92 @@
-package com.biptek.posbiptek.fragment;
-
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.os.Vibrator;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.util.DisplayMetrics;
-import android.util.SparseArray;
-import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.biptek.posbiptek.R;
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
-
-import java.io.IOException;
-
-public class Fragment_BarcodeScanner extends Fragment {
-    SurfaceView surfaceView;
-    CameraSource cameraSource;
-    BarcodeDetector barcodeDetector;
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.barcode_scanner, container, false);
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-
-        surfaceView = view.findViewById(R.id.barcodeScanner);
-        barcodeDetector = new BarcodeDetector.Builder(getActivity()).setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE).build();
-        cameraSource = new
-                CameraSource.Builder(getActivity(), barcodeDetector).setRequestedPreviewSize(displayMetrics.widthPixels, displayMetrics.heightPixels).build();
-
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-
-                try {
-                    cameraSource.start(holder);
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                cameraSource.stop();
-            }
-        });
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                SparseArray<Barcode> barcodeSparseArray = detections.getDetectedItems();
-
-                if(barcodeSparseArray.size() != 0){
-                    surfaceView.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-                            vibrator.vibrate(1000);
-                        }
-                    });
-                }
-            }
-        });
-
-        return view;
-    }
-}
+//package com.biptek.posbiptek.fragment;
+//
+//import android.Manifest;
+//import android.app.Activity;
+//import android.content.Context;
+//import android.content.pm.PackageManager;
+//import android.os.Bundle;
+//import android.os.Vibrator;
+//import android.support.annotation.NonNull;
+//import android.support.annotation.Nullable;
+//import android.support.v4.app.ActivityCompat;
+//import android.support.v4.app.Fragment;
+//import android.util.DisplayMetrics;
+//import android.util.SparseArray;
+//import android.view.LayoutInflater;
+//import android.view.SurfaceHolder;
+//import android.view.SurfaceView;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import com.biptek.posbiptek.R;
+////part of ML kit katanya untuk library qr and barcode scanner
+//
+//import java.io.IOException;
+//
+//public class Fragment_BarcodeScanner extends Fragment {
+//    SurfaceView surfaceView;
+//    //CameraSource cameraSource;
+//    //BarcodeDetector barcodeDetector;
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.barcode_scanner, container, false);
+//        DisplayMetrics displayMetrics = new DisplayMetrics();
+//        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+//
+//        surfaceView = view.findViewById(R.id.barcodeScanner);
+//        barcodeDetector = new BarcodeDetector.Builder(getActivity()).setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE).build();
+//        cameraSource = new
+//                CameraSource.Builder(getActivity(), barcodeDetector).setRequestedPreviewSize(displayMetrics.widthPixels, displayMetrics.heightPixels).build();
+//
+//        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+//            @Override
+//            public void surfaceCreated(SurfaceHolder holder) {
+//                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//                    return;
+//                }
+//
+//                try {
+//                    cameraSource.start(holder);
+//                } catch (IOException e){
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            @Override
+//            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+//
+//            }
+//
+//            @Override
+//            public void surfaceDestroyed(SurfaceHolder holder) {
+//                cameraSource.stop();
+//            }
+//        });
+//
+//        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+//            @Override
+//            public void release() {
+//
+//            }
+//
+//            @Override
+//            public void receiveDetections(Detector.Detections<Barcode> detections) {
+//                SparseArray<Barcode> barcodeSparseArray = detections.getDetectedItems();
+//
+//                if(barcodeSparseArray.size() != 0){
+//                    surfaceView.post(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+//                            vibrator.vibrate(1000);
+//                        }
+//                    });
+//                }
+//            }
+//        });
+//
+//        return view;
+//    }
+//}
--- PosBiptek/app/src/main/java/com/biptek/posbiptek/model/ListProdukRestock.java
+++ PosBiptek/app/src/main/java/com/biptek/posbiptek/model/ListProdukRestock.java
@@ -1,18 +1,33 @@
 package com.biptek.posbiptek.model;
 
 public class ListProdukRestock {
+    private long id_list_produk_restock;
     private long kode_restock_list;
     private String kode_produk_list_restock;
     private int jumlah_produk_restock;
 
-    public ListProdukRestock(long kode_restock_list, String kode_produk_list_restock, int jumlah_produk_restock) {
+    public ListProdukRestock(long id_list_produk_restock, long kode_restock_list, String kode_produk_list_restock, int jumlah_produk_restock) {
+        this.id_list_produk_restock = id_list_produk_restock;
         this.kode_restock_list = kode_restock_list;
+        this.kode_produk_list_restock = kode_produk_list_restock;
+        this.jumlah_produk_restock = jumlah_produk_restock;
+    }
+
+    public ListProdukRestock(String kode_produk_list_restock, int jumlah_produk_restock) {
         this.kode_produk_list_restock = kode_produk_list_restock;
         this.jumlah_produk_restock = jumlah_produk_restock;
     }
 
     public ListProdukRestock() {
 
+    }
+
+    public long getId_list_produk_restock() {
+        return id_list_produk_restock;
+    }
+
+    public void setId_list_produk_restock(long id_list_produk_restock) {
+        this.id_list_produk_restock = id_list_produk_restock;
     }
 
     public long getKode_restock_list() {
