5c1dedfe9d12994b17f8bebeeb75c50d6dc58961
rubythalib33 <abdullah3333abdul@gmail.com>
2019-03-08 08:59:41 +0700

    Manajemen Perusahaan

--- PosBiptek/app/src/main/java/com/biptek/posbiptek/activity/manajemen_toko.java
+++ PosBiptek/app/src/main/java/com/biptek/posbiptek/activity/manajemen_toko.java
@@ -3,13 +3,34 @@ package com.biptek.posbiptek.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.biptek.posbiptek.R;
+import com.biptek.posbiptek.model.CRUD;
+import com.biptek.posbiptek.model.Perusahaan;
 
 public class manajemen_toko extends AppCompatActivity {
+    private CRUD crud;
+    EditText NamaPerusahaan,AlamatLengkapPT,NamaPemilikPT;
+    Button SimpanPT,BatalPT;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manajementoko);
+        crud = new CRUD(this);
+        Perusahaan perusahaan = new Perusahaan();
+        NamaPerusahaan=findViewById(R.id.NamaPerusahaan);
+        AlamatLengkapPT=findViewById(R.id.AlamatLengkapPT);
+        NamaPemilikPT= findViewById(R.id.NamaPemilikPT);
+
+        SimpanPT=findViewById(R.id,SimpanPT);
+
+        crud.open();;
+        //nanti ngambil data di shared preferences
+        perusahaan = crud.getPerusahaan(1);
+        crud.close();
+
+        NamaPerusahaan.setText();
     }
 }
