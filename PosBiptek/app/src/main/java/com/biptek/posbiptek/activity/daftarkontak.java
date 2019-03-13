5e076803ecaaf3e5f235f0ea2a7fdf1f2489c2d8
rubythalib33 <abdullah3333abdul@gmail.com>
2019-03-13 15:00:53 +0700

    modul Supplier2

--- PosBiptek/app/src/main/java/com/biptek/posbiptek/activity/daftarkontak.java
+++ PosBiptek/app/src/main/java/com/biptek/posbiptek/activity/daftarkontak.java
@@ -28,7 +28,7 @@ public class daftarkontak extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.daftarKontak);
+        setContentView(R.layout.daftarkontak);
 
         listSupplier = findViewById(R.id.listviewKontak);
         crud = new CRUD(this);
