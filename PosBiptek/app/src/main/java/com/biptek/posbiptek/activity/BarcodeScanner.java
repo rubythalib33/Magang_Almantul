35ce0b8906797415bf579673486bb111e345412e
TaufiqOdhi <taufiq_0dhi_dwi_putra@student.uns.ac.id>
2019-03-22 18:00:31 +0700

    bacrode scanner-camera permission request for SDK 23 and higher

--- PosBiptek/app/src/main/java/com/biptek/posbiptek/activity/BarcodeScanner.java
+++ PosBiptek/app/src/main/java/com/biptek/posbiptek/activity/BarcodeScanner.java
@@ -1,6 +1,10 @@
 package com.biptek.posbiptek.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,8 +24,18 @@ public class BarcodeScanner extends AppCompatActivity implements ZXingScannerVie
 
         mScannerView = new ZXingScannerView(this);
         setContentView(mScannerView);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 2);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            mScannerView.setResultHandler(this);
+            mScannerView.startCamera();
+        }
+        else finish();
+    }
 
     @Override
     public void handleResult(Result result) {
